2cae144c6ebeb7b96d5c793155428219babf7e6e
Jonathan McClen <jonathangmcclen@gmail.com>
2025-01-30 20:53:31 +0000

    Refactor memory allocation and image generation.

--- Mandlebrot Explorer/main.cpp
+++ Mandlebrot Explorer/main.cpp
@@ -3,7 +3,6 @@
 #include <cuda_runtime.h>
 #include "device_launch_parameters.h"
 
-
 // Image array parameters
 const int arr_width = 100;
 const int arr_height = 100;
@@ -14,25 +13,33 @@ int size_of_arr_bytes = sizeof(int) * size_of_arr;
 int* imagePtr = new int[size_of_arr];
 int* imagePtr_CUDA;
 
+dim3 threadsPerBlock(10, 10);
+dim3 blocksPerGrid((arr_width + threadsPerBlock.x - 1) / threadsPerBlock.x,
+    (arr_height + threadsPerBlock.y - 1) / threadsPerBlock.y);
+
 void cleanupMemory() {
     // Cleanup memory
     cudaFree(imagePtr_CUDA);
     delete[] imagePtr;
 }
 
-int main() {
-
+void allocateCUDAMemory() {
     cudaMalloc((void**)&imagePtr_CUDA, size_of_arr_bytes);
+}
 
-    // Run kernel
-    dim3 threadsPerBlock(10, 10);
-    dim3 blocksPerGrid((arr_width + threadsPerBlock.x - 1) / threadsPerBlock.x,
-        (arr_height + threadsPerBlock.y - 1) / threadsPerBlock.y);
+void generateMandlebrotImage() {
+
+    // Generate image
     mandelbrot_kernel << <blocksPerGrid, threadsPerBlock >> > (imagePtr_CUDA, arr_width, arr_height);
-
     // Copy array to host(CPU) memory
     cudaMemcpy(imagePtr, imagePtr_CUDA, size_of_arr_bytes, cudaMemcpyDeviceToHost);
 
+}
+
+int main() {
+
+    allocateCUDAMemory();
+    generateMandlebrotImage();
 
     // Output the image to the console (for debugging)
     for (int y = 0; y < arr_height; ++y) {
