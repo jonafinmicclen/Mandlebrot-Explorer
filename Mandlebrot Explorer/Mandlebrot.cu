591cea0853f01dceaad1b46e803d7a9c24372dd8
Jonathan McClen <jonathangmcclen@gmail.com>
2025-02-10 15:21:34 +0000

    Max iter based on edge detection

--- Mandlebrot Explorer/Mandlebrot.cu
+++ Mandlebrot Explorer/Mandlebrot.cu
@@ -25,22 +25,37 @@ __global__ void mandelbrot_kernel(int* imagePtr, int width, int height,
                 y_offset + (y - (height/2))/y_zoom  );
 
     int* pixelPtr = &imagePtr[y * width + x];
-   
-    // Iterations optimiser, use convolution of near pixel to calculate max iterations frame will converge to true mandlebrot 
-    
 
     int neighbour_sum = 0;
-    int i = 0;
-    int convolution_width = 2;
-    for (int iX = max(x - convolution_width, 0); iX <= min(x + convolution_width, width); ++iX) {
-        for (int iY = max(y - convolution_width, 0); iY <= min(y + convolution_width, height); ++iY)
-        {
-            neighbour_sum += imagePtr[iY * width + iX] * imagePtr[iY * width + iX];
-            ++i;
+    int Gx[3][3] = {
+    {-1, 0, 1},
+    {-2, 0, 2},
+    {-1, 0, 1}
+    };
+
+    int Gy[3][3] = {
+        {-1, -2, -1},
+        { 0,  0,  0},
+        { 1,  2,  1}
+    };
+
+    int sumX = 0;
+    int sumY = 0;
+
+    for (int iX = -1; iX <= 1; ++iX) {
+        for (int iY = -1; iY <= 1; ++iY) {
+            int sampleX = min(max(x + iX, 0), width - 1);
+            int sampleY = min(max(y + iY, 0), height - 1);
+            int pixel = imagePtr[sampleY * width + sampleX];
+
+            sumX += pixel * Gx[iY + 1][iX + 1];
+            sumY += pixel * Gy[iY + 1][iX + 1];
         }
     }
-    neighbour_sum = sqrtf(neighbour_sum);
-    int zoom_factor = 5 * (neighbour_sum * sqrt(x_zoom) / 10) + 1000;
+
+    // Compute gradient magnitude
+    neighbour_sum = sqrtf(sumX * sumX + sumY * sumY);
+    int zoom_factor = (5 * (sqrt(x_zoom) / 10) + 1000) + (neighbour_sum * neighbour_sum * sqrt(x_zoom)/20000);
 
     *pixelPtr = mandlebrot_iterate(C, zoom_factor);
 }
