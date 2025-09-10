42532f7eb191d15b6bd1b85e98d521cbdf2e586c
Jonathan McClen <jonathangmcclen@gmail.com>
2025-09-10 02:05:37 +0100

    Render all Zn

--- Mandlebrot Explorer/Mandlebrot.cu
+++ Mandlebrot Explorer/Mandlebrot.cu
@@ -3,39 +3,27 @@
 __device__ void mandlebrot_iterate(Complex C, int* imagePtr, float x_zoom, float y_zoom, float x_offset, float y_offset)
 {
     Complex Zn = Complex(0, 0);
-    Complex complexPoints[MAX_ITERATIONS];
-
     int iterations_countdown = MAX_ITERATIONS;
-
     while (--iterations_countdown)
     {
-        if (Zn.magnitude() >= 2.0) 
-        { 
 
-            for (int i = MAX_ITERATIONS - 1; i > iterations_countdown + 1; --i) 
-            {
-                // Convert Complex positions back to array coordinates
-                int x = complexPoints[i].real * x_zoom - x_offset + ARR_WIDTH / 2;
-                int y = complexPoints[i].imag * y_zoom - y_offset + ARR_HEIGHT / 2;
+        // Convert Complex positions back to array coordinates
+        int x = Zn.real * x_zoom - x_offset + ARR_WIDTH / 2;
+        int y = Zn.imag * y_zoom - y_offset + ARR_HEIGHT / 2;
 
-                int imag_idx = y * ARR_WIDTH + x;
+        int imag_idx = y * ARR_WIDTH + x;
 
-                // Ensure in bounds then add
-                if (imag_idx >= 0 && imag_idx < ARR_SIZE) {
-                    atomicAdd(&imagePtr[imag_idx], 1);
-                }
-                
-            }
-            return;
+        // Ensure in bounds then add
+        if (imag_idx >= 0 && imag_idx < ARR_SIZE) {
+            atomicAdd(&imagePtr[imag_idx], 1);
         }
 
-        // Store point
-        complexPoints[iterations_countdown] = Zn;
+        if (Zn.magnitude() >= 2.0) { return; }
+
         // Iterate mandlebrot function
         Zn = Zn * Zn + C;
     }
     return;
-
 }
 
 
